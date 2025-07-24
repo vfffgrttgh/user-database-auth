4718d7a020c589fc0133fe2eaa23e174c875859f
Aayansh13 <163544061+vfffgrttgh@users.noreply.github.com>
2025-07-24 21:04:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ function decryptMsg(encryptedstuff) {
         return decrypted;
     } catch(err) {
         console.error(err);
+        return "DECRYPTED [ERROR]"
     }
 }
 
@@ -177,6 +178,6 @@ io.on("connection", socket => {
     });
 });
 
-server.listen(PORT,"0.0.0.0", () => {
+server.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
 });
